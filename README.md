b301818552bfba0a27563fbe530c5cd158922161
Ben White <ben@gosquared.com>
2016-01-09 14:33:34 +0000

    Removed Promises. Tidied up tests Improved Readme

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
 # Hyacinth
 
-### Actually, it's a Token **Bouquet** rate limiter
+> _Actually, it's a Token **Bouquet** rate limiter_
+
+#### Atomic, distributed rate limiting using a token bucket algorithm.
+
+Token Bucket rate-limiting is designed to try and reduce spikey traffic to your service. There are a few existing libraries that implement this but none that ensure atomicity across distributed services.
+
+Hyacinth uses Redis as it's shared store and Lua scripting to ensure atomicity between clients.
+
 
 ## Installation
 
 When its on npm...
 
 ```sh
-npm install hyacinth
+npm install hyacinth --save
 ```
 
 ## Usage
@@ -19,7 +26,8 @@ var rateLimiter = new TokenBucket({
 	redis: redisClient
 });
 
-rateLimiter.rateLimit(testKey, 10, 250, 240).then(function(tokensRemaining){
+// On each request to the resource
+rateLimiter.rateLimit(resourceKey, 10, 250, 240, function(err, tokensRemaining){
     // Negative number indicates the tokens remaining but limited
     // as the cost was higher than those remaining
 
@@ -28,6 +36,49 @@ rateLimiter.rateLimit(testKey, 10, 250, 240).then(function(tokensRemaining){
 });
 ```
 
-## Full API
 
-### Coming soon
+### Express middleware example
+
+```js
+
+var Hyacinth = require('hyacinth');
+var Redis = require('redis');
+var tokenBucket new Hyacinth({
+  redis: Redis.createClient()
+});
+
+module.exports = function rateLimits(req, res, next) {
+  var key = 'API:limits:' + req.userID;   // Create a key that is associated with the API user
+  var cost = 10;                          // This could be set dependent on the endpoint route
+  var poolMax = 250;                      // The number of tokens for the user
+  var fillInterval = 100;                 // How often a token is added to the bucket
+
+  // This config will allow an average 10 requests per second
+  // and bursts of 25 requests with a full token bucket
+
+  tokenBucket.rateLimit(key, cost, poolMax, fillInterval, function(err, tokensRemaining) {
+    if (err) {
+      // Indicated a problem with your Redis connection / configuration
+      return next(new Error('InternalError'));
+    }
+
+    res.setHeader('X-RateLimit-Limit', poolMax);
+    res.setHeader('X-RateLimit-Remaining', tokensRemaining);
+    res.setHeader('X-RateLimit-Cost', cost);
+
+    if (tokensRemaining < 0) {
+      // A negative response indicates that they should be rate limited
+      console.log(req.userID ' has been rate limited.');
+
+      res.status(429).send({
+        message: 'Your request has been rate limited'
+      });
+
+      return;
+    }
+
+    next();
+  });
+
+
+```
--- test/TokenBucket.spec.js
+++ test/TokenBucket.spec.js
@@ -1,116 +1,114 @@
 var chai = require('chai');
+var async = require('async');
 var expect = chai.expect;
 
 var Redis = require('redis');
 var TokenBucket = require('../lib/TokenBucket');
 
 var rateLimiter;
-var timers = 0;
-var count = 0;
 var client;
 
 
-describe('TokenBucket', function(){
+describe('TokenBucket', function() {
 
-	before(function(done){
+  before(function(done) {
+    client = Redis.createClient(6379, 'localhost');
+    client.on('error', function(err) {
+      console.error('Error on redis connection', err);
+    });
 
-		client = Redis.createClient(6379, 'localhost');
-		client.on('error', function(err) {
-			log.error('Error on redis connection');
-		});
+    client.on('ready', function() {
+      rateLimiter = new TokenBucket({ redis: client });
+      done();
+    });
+  });
 
-		client.on('ready', function(){
-      rateLimiter = new TokenBucket({redis:client});
-			done();
-		});
-	});
+  describe('rateLimitReset', function() {
+    it('should return true when resetting', function(done) {
 
-	describe('rateLimitReset', function(){
-		it('should return true when resetting', function(){
+      var testKey = 'API:limits:testing:0:';
 
-			var testKey = 'API:limits:testing:0:';
+      rateLimiter.clearRateLimitWithKey(testKey, function(err, data) {
+        expect(err).to.equal(null);
+        expect(data).to.be.true;
+        done();
+      });
+    });
+  });
 
-			return rateLimiter.clearRateLimitWithKey(testKey).then(function(data){
-				expect(data).to.be.true;
-			});
-		});
-	});
+  describe('rateLimit', function() {
 
-	describe('rateLimit', function(){
+    beforeEach(function(done) {
+      client.eval('return redis.call("del", unpack(redis.call("keys", KEYS[1])))', 1, 'API:limits:testing:*', function(err, res) {
+        done();
+      });
+    });
 
-		beforeEach(function(done){
-			client.eval('return redis.call("del", unpack(redis.call("keys", KEYS[1])))', 1, 'API:limits:testing:*', function(err, res){
-				done();
-			});
-		});
+    it('should return the the pool max minus the cost after being reset', function(done) {
+      var testKey = 'API:limits:testing:1:';
 
-		it('should return the the pool max minus the cost after being reset', function(){
+      rateLimiter.rateLimit(testKey, 10, 250, 240, function(err, data) {
+        console.log(data);
+        expect(data).to.equal(240);
+        done();
+      });
+    });
 
-			var testKey = 'API:limits:testing:1:';
+    //
+    // Numeric tests
+    //
 
-			return rateLimiter.rateLimit(testKey, 10, 250, 240).then(function(data){
-				expect(data).to.equal(240);
-			});
-		});
+    it('should allow 250 hits out of 250 over 2 seconds at a cost of 1', function(done) {
+      this.timeout(4000);
 
-		//
-		// Numeric tests
-		//
+      testRateLimit(250, 240, 250, 2000, 1, function(err, data) {
+        var passed = data.filter(function(item) {return item >= 0;}).length;
+        expect(passed).to.equal(250);
+        done();
+      });
+    });
 
-		it('should allow 250 hits out of 250 over 2 seconds at a cost of 1', function(){
-			this.timeout(4000);
+    it('should allow 172 hits out of 250 over 2 seconds at a cost of 1.5', function(done) {
+      this.timeout(4000);
 
-			return testRateLimit(250, 240, 250, 2000, 1).then(function(data){
-				var passed = data.filter(function(item){return item >= 0;}).length;
-				expect(passed).to.equal(250);
-			});
-		});
+      testRateLimit(250, 240, 250, 2000, 1.5, function(err, data) {
+        var passed = data.filter(function(item) {return item >= 0;}).length;
+        expect(passed).to.equal(172);
+        done();
+      });
+    });
 
-    it('should allow 172 hits out of 250 over 2 seconds at a cost of 1.5', function(){
-			this.timeout(4000);
+    it('should allow 258 hits out of 500 over 2 seconds at a cost of 1', function(done) {
+      this.timeout(4000);
 
-			return testRateLimit(250, 240, 250, 2000, 1.5).then(function(data){
-				var passed = data.filter(function(item){return item >= 0;}).length;
-				expect(passed).to.equal(172);
-			});
-		});
+      testRateLimit(250, 240, 500, 2000, 1, function(err, data) {
+        var passed = data.filter(function(item) {return item >= 0;}).length;
+        expect(passed).to.equal(258);
+        done();
+      });
+    });
 
-		it('should allow 258 hits out of 500 over 2 seconds at a cost of 1', function(){
-			this.timeout(4000);
+    it('should allow 254 hits out of 500 over 1 seconds at a cost of 1', function(done) {
+      this.timeout(4000);
 
-			return testRateLimit(250, 240, 500, 2000, 1).then(function(data){
-				var passed = data.filter(function(item){return item >= 0;}).length;
-				expect(passed).to.equal(258);
-			});
-		});
-
-		it('should allow 254 hits out of 500 over 1 seconds at a cost of 1', function(){
-			this.timeout(4000);
-
-			return testRateLimit(250, 240, 500, 1000, 1).then(function(data){
-				var passed = data.filter(function(item){return item >= 0;}).length;
-				expect(passed).to.equal(254);
-			});
-		});
-
-	});
+      testRateLimit(250, 240, 500, 1000, 1, function(err, data) {
+        var passed = data.filter(function(item) {return item >= 0;}).length;
+        expect(passed).to.equal(254);
+        done();
+      });
+    });
+  });
 });
 
-function testRateLimit(poolMax, fillRate, hits, time, cost) {
+function testRateLimit(poolMax, fillRate, hits, time, cost, cb) {
+  //Expected pass amount is poolMax + time(ms) / fillRate / cost
 
-	//Expected pass amount is poolMax + time(ms) / fillRate / cost
+  var functions = [];
+  var key = 'API:limits:testing:';
 
-	var promises = [];
-
-	var key = 'API:limits:testing:';
-
-	for (var i =0; i < hits; i += 1){
-		promises.push(new Promise(function(resolve, reject) {
-			setTimeout(function(){
-				rateLimiter.rateLimit(key, cost, poolMax, fillRate).then(resolve).catch(reject);
-			}, (time / hits) * i);
-		}));
-	};
-
-	return Promise.all(promises);
+  async.times(hits, function(i, done) {
+    setTimeout(function() {
+      rateLimiter.rateLimit(key, cost, poolMax, fillRate, done);
+    }, (time / hits) * i);
+  }, cb);
 }
