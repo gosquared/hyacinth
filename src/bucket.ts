0d311cef0dcdaf7da70e269e8b48580ed0782555
Geoff Wagstaff <geoff@gosquared.com>
2021-04-18 20:09:21 +0000

    balance

--- src/bucket.ts
+++ src/bucket.ts
@@ -1,4 +1,4 @@
-import IORedis from 'ioredis'
+import IORedis, { Redis } from 'ioredis'
 
 import { createScript } from 'node-redis-script'
 import fs from 'fs'
@@ -7,12 +7,25 @@ import path from 'path'
 const fp = path.resolve(path.join('./deduct.lua'))
 const src = fs.readFileSync(fp)
 
-export async function deductTokens (
-  ioredis: IORedis.Redis,
-  name: string,
-  num: number,
-  max: number,
+export default class TokenBucket {
+  ioredis: IORedis.Redis
+  name: string
+  max: number
   fill: number
-) {
 
+  constructor (
+    ioredis: IORedis.Redis,
+    name: string,
+    max: number,
+    fill: number
+  ) {
+    this.ioredis = ioredis
+    this.name = name
+    this.max = max
+    this.fill = fill
+  }
+
+  async balance () {
+    return this.ioredis.get(`${this.name}.pool`)
+  }
 }
